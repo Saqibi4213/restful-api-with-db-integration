a0ca378c3cfe951a5211bc2b56f63af5269ad643
Saqibi4213 <mehriamosawer@gmail.com>
2024-10-15 20:32:01 +0430

    erro in router.js handled

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
