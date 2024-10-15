6285060888ac7693daf2a58c1b56b8ede9d8fdd0
Saqibi4213 <mehriamosawer@gmail.com>
2024-10-15 20:15:32 +0430

    solving error in db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://mehr1234:mehr1234@node-project.dsot9.mongodb.net/?retryWrites=true&w=majority&appName=node-project");
-    console.log("Connected to MongoDB successfully.");
+    await mongoose.connect('mongodb+srv://mehr1234:mehr1234@node-project.dsot9.mongodb.net/?retryWrites=true&w=majority&appName=node-project');
+    console.log('Connected to MongoDB successfully.');
   } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
+    console.error('Failed to connect to MongoDB:', error);
   }
 };
 
