6019f4838b2690fbad1e72577bcf6a92112b534f
Saqibi4213 <mehriamosawer@gmail.com>
2024-10-15 20:23:48 +0430

    errors handling in server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
+const express = require('express');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+
 const app = express();
 const port = 3000;
 
 connectDB();
 
 app.use(express.json());
-app.use("/users", userRoutes);
+app.use('/users', userRoutes);
 
-app.get("/", (_req, res) => {
-  res.send("Welcome to the RESTful API!!!");
+app.get('/', (_req, res) => {
+  res.send('Welcome to the RESTful API!!!');
 });
 
 app.listen(port, () => {
